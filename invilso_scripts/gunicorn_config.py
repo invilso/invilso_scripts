0fdd543c370e1cc2c67005d34e465dd51e31da9b
invilso <invilsomail@gmail.com>
2023-04-23 14:10:09 +0300

    some changes

--- invilso_scripts/gunicorn_config.py
+++ invilso_scripts/gunicorn_config.py
@@ -1,7 +1,7 @@
 command = '/usr/bin/gunicorn'
 pythonpath = '/root/site/invilso_scripts'
-bind = '127.0.0.1:8005'
-workers = 3
+bind = '127.0.0.1:8000'
+workers = 9
 user = 'root'
 limit_request_fields = 32000
 limit_request_field_size = 0
--- invilso_scripts/settings.py
+++ invilso_scripts/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['*']
 MEDIA_ROOT = 'uploads/'
 LOGIN_REDIRECT_URL = '/'
 
